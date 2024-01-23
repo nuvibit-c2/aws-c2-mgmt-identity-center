c3f48d5a7920a6f5b442eb3cc8f926275faf17d0
stefano-franco <stefano.franco@nuvibit.com>
2024-01-23 15:14:41 +0100

    update parameters

--- ntc_parameters.tf
+++ ntc_parameters.tf
@@ -5,7 +5,7 @@ locals {
   ntc_parameters_to_write = {}
 
   # by default existing node parameters will be merged with new parameters to avoid deleting parameters
-  replace_parameters = true
+  ntc_replace_parameters = true
 
   # map of parameters merged from all parameter nodes
   ntc_parameters = module.ntc_parameters_reader.all_parameters
@@ -30,9 +30,10 @@ module "ntc_parameters_reader" {
 module "ntc_parameters_writer" {
   source = "github.com/nuvibit-terraform-collection/terraform-aws-ntc-parameters//modules/writer?ref=1.1.0"
 
-  bucket_name     = local.ntc_parameters_bucket_name
-  parameter_node  = local.ntc_parameters_writer_node
-  node_parameters = local.ntc_parameters_to_write
+  bucket_name        = local.ntc_parameters_bucket_name
+  parameter_node     = local.ntc_parameters_writer_node
+  node_parameters    = local.ntc_parameters_to_write
+  replace_parameters = local.ntc_replace_parameters
 
   providers = {
     aws = aws.euc1
